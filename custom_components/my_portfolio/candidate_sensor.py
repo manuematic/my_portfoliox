0d9a8670b3af0baa0ca2285b891b8739515809ef
Martin N. <manuematic@gmail.com>
2026-04-23 17:23:14 +0200

    0.9.8a Fehler behoben

--- custom_components/my_portfolio/candidate_sensor.py
+++ custom_components/my_portfolio/candidate_sensor.py
@@ -5,7 +5,8 @@ from typing import Any
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity import DeviceInfo, DeviceEntryType
+from homeassistant.helpers.entity import DeviceInfo
+from homeassistant.helpers.device_registry import DeviceEntryType
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import (
